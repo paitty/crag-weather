25541ee972751bb20e882885e48f71bb95c7737e
PierreAdrienItty <122536463+PierreAdrienItty@users.noreply.github.com>
2025-04-08 15:10:50 +0200

    adding logs for pollen

--- generate_crag_weather_html.py
+++ generate_crag_weather_html.py
@@ -305,3 +305,57 @@ createTable()
 html = soup.prettify("utf-8")
 with open("build_outputs_folder/index.html", "wb") as file:
     file.write(html)
+
+
+
+headers = {'Accept':	'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Encoding':	'gzip, deflate, br, zstd',
+    'Accept-Language':	'en-US,fr;q=0.8,hr;q=0.5,en;q=0.3',
+    'Connection':	'keep-alive',
+    'DNT':	'1',
+    'Priority':	'u=0, i',
+    'Sec-Fetch-Dest':	'document',
+    'Sec-Fetch-Mode':	'navigate',
+    'Sec-Fetch-Site':	'none',
+    'Sec-Fetch-User':	'?1',
+    'Sec-GPC':	'1',
+    'sitename': 'https://github.com/paitty/crag-weather.git',
+    'Upgrade-Insecure-Requests':	'1',
+    'User-Agent':	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:135.0) Gecko/20100101 Firefox/135.0'}
+
+url = "https://www.plivazdravlje.hr/alergije/prognoza/1/zagreb.html"
+r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+soup = BeautifulSoup(r.content,"html.parser")
+
+#now2 = now2 + timedelta(days=-1)
+today_date = now2.strftime("%d/%m/%Y")
+
+
+
+pollen_table={}
+
+with open('pollen-table.json') as f:
+    pollen_table = json.load(f)
+
+for soup3 in soup.find_all('div'):
+    #print(soup3.get("class"))
+    if soup3.get("class"):
+        if soup3.get("class")[0] == "peludHolder":
+            for soup2 in soup3.find_all('div'):
+                if soup2.get("class")[0] == "peludKategorija":
+                    break
+            for soup1 in soup3.find_all('span'):
+                if soup1.get("class"):
+                    if soup1.get("class")[0] == "peludStupacOcitanje":
+                        break
+            if soup2.text.strip() not in pollen_table.keys():
+                pollen_table[soup2.text.strip()]={}
+            pollen_table[soup2.text.strip()][today_date] = soup1.text.strip()
+            print(soup2.text.strip()+": "+soup1.text.strip())
+
+with open('pollen-table.json', 'w') as f:
+    json_pretty = json.dumps(pollen_table, indent=2)
+    f.write(json_pretty)
+
+pass
+
