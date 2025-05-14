f9f12a97787dfbb31e97fcd1010cb7b11c175d57
PierreAdrienItty <122536463+PierreAdrienItty@users.noreply.github.com>
2025-05-14 11:27:50 +0200

    Update pollen code
    + All dates are now taken into account
    + Pollen page is now separate

---
+++ generate_pollen_html.py
@@ -0,0 +1,139 @@
+import json
+from bs4 import BeautifulSoup
+from datetime import datetime, timezone, timedelta
+import requests
+import numpy
+
+
+def generate_pollen_table():
+    def createPollenTable():
+        new_table=soup.new_tag("table")
+        new_table.attrs['class']='myTable'
+        new_table.attrs['id']='myTable2'
+        soup.html.body.append(new_table)
+        display_table = soup.html.body.find_all("table")[-1]
+
+        table_names=list(pollen_table.keys())
+        table_names.insert(0,'date')
+        new_header = soup.new_tag("tr")
+        display_table.append(new_header)
+        for col in table_names:
+            new_column_header = soup.new_tag("th")
+            new_column_header.string=col
+            display_table.tr.append(new_column_header)
+
+        date_list=[]
+        for tree in pollen_table.keys():
+            date_list = date_list + list(set(pollen_table[tree].keys()) - set(date_list))
+
+        date_num_list=[]
+        for date in date_list:
+            new_date = datetime.strptime(date, "%d/%m/%Y")
+            new_date = new_date.strftime("%Y%m%d")
+            date_num_list = date_num_list+[new_date]
+        
+        sorted_date_num_list=sorted(date_num_list)
+
+        sorted_date_list=[]
+        for date in sorted_date_num_list:
+            new_date = datetime.strptime(date, "%Y%m%d")
+            new_date = new_date.strftime("%d/%m/%Y")
+            sorted_date_list = sorted_date_list+[new_date]   
+        
+        for date in sorted_date_list:
+            new_line = soup.new_tag("tr")
+            display_table.append(new_line)
+            last_line=display_table.find_all("tr")[-1]
+            for col in table_names:
+                if col == "date":
+                    new_tag=soup.new_tag('td')
+                    new_tag.string=date
+                else:
+                    new_tag=soup.new_tag('td')
+                    if date in pollen_table[col].keys():
+                        new_tag.string=pollen_table[col][date].replace(".",",")
+                    else:
+                        new_tag.string=""
+                last_line.append(new_tag)
+
+    headers = {'Accept':	'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Encoding':	'gzip, deflate, br, zstd',
+        'Accept-Language':	'en-US,fr;q=0.8,hr;q=0.5,en;q=0.3',
+        'Connection':	'keep-alive',
+        'DNT':	'1',
+        'Priority':	'u=0, i',
+        'Sec-Fetch-Dest':	'document',
+        'Sec-Fetch-Mode':	'navigate',
+        'Sec-Fetch-Site':	'none',
+        'Sec-Fetch-User':	'?1',
+        'Sec-GPC':	'1',
+        'sitename': 'https://github.com/paitty/crag-weather.git',
+        'Upgrade-Insecure-Requests':	'1',
+        'User-Agent':	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:135.0) Gecko/20100101 Firefox/135.0'}
+
+    url = "https://www.plivazdravlje.hr/alergije/prognoza/1/zagreb.html"
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(r.content,"html.parser")
+
+    # datetime object containing current date and time
+    now = datetime.now()
+
+    today_date = now.strftime("%d/%m/%Y")
+
+    pollen_table={}
+
+    with open('pollen-table.json') as f:
+        pollen_table = json.load(f)
+
+    for soup3 in soup.find_all('div'):
+        #print(soup3.get("class"))
+        if soup3.get("class"):
+            if soup3.get("class")[0] == "peludHolder":
+                for soup2 in soup3.find_all('div'):
+                    if soup2.get("class")[0] == "peludKategorija":
+                        break
+                for soup1 in soup3.find_all('span'):
+                    if soup1.get("class"):
+                        if soup1.get("class")[0] == "peludStupacOcitanje":
+                            break
+                if soup2.text.strip() not in pollen_table.keys():
+                    pollen_table[soup2.text.strip()]={}
+                pollen_table[soup2.text.strip()][today_date] = soup1.text.strip()
+                print(soup2.text.strip()+": "+soup1.text.strip())
+
+    with open('pollen-table.json', 'w') as f:
+        json_pretty = json.dumps(pollen_table, indent=2)
+        f.write(json_pretty)
+
+
+    pass
+    HTML_DOC = """<!DOCTYPE html>
+    <html>
+    <head>
+        <style type="text/css">
+            .myTable { background-color:#eee;border-collapse:collapse; }
+
+            .myTable td, .myTable th { padding:5px;border:1px solid #000; }
+        </style>
+        <title>Pollen in Zagreb</title>
+    </head>
+    <body>
+        <p>
+            Na temelju <a href="https://www.plivazdravlje.hr/alergije/prognoza/1/zagreb.html"> Pliva</a>
+        </p>
+        <br>
+    </body>
+    </html>
+    """
+
+    soup = BeautifulSoup(HTML_DOC, "html.parser")
+
+    createPollenTable()
+
+    html = soup.prettify("utf-8")
+    with open("build_outputs_folder/pollen.html", "wb") as file:
+        file.write(html)
+
+if __name__ == "__main__":
+    print("This is a different version of the module.py file.")
+    generate_pollen_table()
